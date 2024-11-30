8c76237ec9cd3ecfc27aac1e7fde741292df3bf0
ChiaYing0 <vivian787678@gmail.com>
2024-11-30 19:09:58 +0800

    add antd; modify UI/layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import SelectionUI from './SelectionUI';
+import { Flex, Button } from 'antd';
+
+const accent_options = [
+  {value: '日本'},
+  {value: '越南'},
+  {value: '法國'},  
+  {value: '印度'},
+  {value: '澳洲'},
+];
+
+const scenario_options = [
+  {value: '旅遊'},
+  {value: '面試'},
+  {value: '日常'},
+];
 
 function App() {
+
+  const [accent, setAccent] = useState("choose");
+  const [scenario, setScenario] = useState("choose");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <div className="Setting">
+
+      <Flex gap='middle' vertical>
+
+      <h1 className="Logo">Tutor</h1>
+
+        <Flex gap="middle" horizontal>
+          <SelectionUI
+            title={"語調"}
+            default_value={accent}
+            options={accent_options}
+            handleChange={setAccent}
+          ></SelectionUI>
+
+          <SelectionUI
+            title={"情境"}
+            default_value={scenario}
+            options={scenario_options}
+            handleChange={setScenario}
+          ></SelectionUI>
+        </Flex>
+      
+        <Button type="primary">Primary Button</Button>
+      
+        </Flex>
+
+      </div>
+
+
+    {/* <h1>{accent}</h1>
+    <h1>{scenario}</h1> */}
     </div>
   );
 }
---
+++ src/SelectionUI.js
@@ -0,0 +1,28 @@
+import { Select, Space } from 'antd';
+
+function SelectionUI({
+    title,
+    default_value,
+    options,
+    handleChange
+}) {
+
+    const onChange = (value) => {
+        handleChange(value);
+    }
+
+    return (
+        <div>
+            <h1>{title}</h1>
+            <Select
+            defaultValue={default_value}
+            style={{ width: 120 }}
+            onChange={onChange}
+            options={options}
+            />
+        </div>
+
+    )
+}
+
+export default SelectionUI;
